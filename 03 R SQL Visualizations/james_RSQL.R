ca4705439ec4b3a6f08f7af2d9674b9a794a4a9a
unknown <james_h.thorpe@yahoo.com>
2015-11-16 08:14:48 -0600

    I finally managed to figure out how to get the cross-tab to work with the joins. Going to pretty it up, and then add it into the Dev_Tracking. Sorry for taking so long, I've just hit roadblock after roadblock.

--- 03 R SQL Visualizations/james_RSQL.R
+++ 03 R SQL Visualizations/james_RSQL.R
@@ -9,6 +9,8 @@ KPI_Medium_Max_value = 1.20
 #getting data for Germany, filter by 
 data_GER <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query="""select Asylum_Country, Origin_Country, sum(Total_Population) as Tot_Ger, sum(Asylum_Seekers) as Asy_Ger, sum(Refugees) as Ref_Ger from REFUGEE_STATS where Asylum_Country = \\\'Germany\\\' group by Asylum_Country, Origin_Country; """')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE))); View(data_GER)
 
+data_GER_test <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query="""select Asylum_Country, Origin_Country, sum(Total_Population) as Tot_Ger, sum(Asylum_Seekers) as Asy_Ger, sum(Refugees) as Ref_Ger, (sum()/ from REFUGEE_STATS where Asylum_Country = \\\'Germany\\\' or Asylum_Country = \\\'Germany\\\' group by Asylum_Country, Origin_Country; """')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE))); View(data_GER)
+
 #aggregate over the years
 #data_test <- data_GER %>% select (as.numeric(as.numeric(levels(ASY_GER)[ASY_GER])))
 
@@ -20,8 +22,10 @@ data_USA <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.ute
 data_all <- dplyr::inner_join(data_GER, data_USA, by = "ORIGIN_COUNTRY")
 
 #data_USA <- data_USA %>% group_by(ORIGIN_COUNTRY) %>% summarize(total_USA = sum(TOT_USA))
-data_final <- data_all %>% select (ORIGIN_COUNTRY, ASY_GER, REF_GER, ASY_USA, REF_USA) %>% filter (as.numeric(as.character(ASY_GER)) > -1, as.numeric(as.character(ASY_USA)) > -1, as.numeric(as.character(REF_GER)) > -1, as.numeric(as.character(REF_USA)) > -1) %>% mutate("ref per as GER" = as.numeric(as.character(REF_GER))/as.numeric(as.character(ASY_GER)), "ref per asy USA" = as.numeric(as.character(REF_USA)) / as.numeric(as.character(ASY_USA)) ) %>% arrange(ORIGIN_COUNTRY) %>% View()
-#get only the rows we care about, and mutate in 
+data_final <- data_all %>% select (ORIGIN_COUNTRY, ASY_GER, REF_GER, ASY_USA, REF_USA) %>% filter (as.numeric(as.character(ASY_GER)) > -1, as.numeric(as.character(ASY_USA)) > -1, as.numeric(as.character(REF_GER)) > -1, as.numeric(as.character(REF_USA)) > -1) %>% mutate("ref_per_asy_GER" = as.numeric(as.character(REF_GER))/as.numeric(as.character(ASY_GER)), "ref_per_asy_USA" = as.numeric(as.character(REF_USA)) / as.numeric(as.character(ASY_USA)) ) %>% arrange(ORIGIN_COUNTRY) %>% filter("ref_per_asy_GER" > -1, "ref_per_asy_USA" > -1) %>% View()
+#get only the rows we care about, and mutate in KPI
+
+data_final <- data_final %>% mutate (KPI = ifelse((ref_per_asy_GER/ref_per_asy_USA) < KPI_Low_Max_value, "GER", ifelse ((ref_per_asy_GER/ref_per_asy_USA) < KPI_Medium_Max_value, "Neutral", ifelse((ref_per_asy_GER/ref_per_asy_USA) > KPI_Medium_Max_value, "USA", "N/A"))))
 
 spread(df, COLOR, SUM_PRICE) %>% View
 
@@ -32,7 +36,7 @@ ggplot() +
   labs(title='Asylum Preference of US vs Germany by Origin') +
   labs(x=paste("Values"), y=paste("ORIGIN_COUNTRY")) +
   layer(data=data_final, 
-        mapping=aes(x=as.character(ASY_GER), y=as.character(ORIGIN_COUNTRY), label=as.character(ASY_GER)), 
+        mapping=aes(x="ASY_GER", y=as.character(ORIGIN_COUNTRY), label=as.character(ASY_GER)), 
         stat="identity", 
         stat_params=list(), 
         geom="text",
@@ -40,23 +44,95 @@ ggplot() +
         position=position_identity()
   ) +
   layer(data=data_final, 
-        mapping=aes(x=as.character(REF_GER), y=as.character(ORIGIN_COUNTRY), label=as.character(REF_GER)), 
+        mapping=aes(x="REF_GER", y=as.character(ORIGIN_COUNTRY), label=as.character(REF_GER)), 
         stat="identity", 
         stat_params=list(), 
         geom="text",
         geom_params=list(colour="black", vjust=2), 
         position=position_identity()
   ) +
-  layer(data=df, 
-        mapping=aes(x=COLOR, y=CLARITY, label=round(RATIO, 2)), 
+  layer(data=data_final, 
+        mapping=aes(x="ASY_USA", y=as.character(ORIGIN_COUNTRY), label=as.character(ASY_USA)), 
         stat="identity", 
         stat_params=list(), 
         geom="text",
         geom_params=list(colour="black", vjust=4), 
         position=position_identity()
   ) +
-  layer(data=df, 
-        mapping=aes(x=COLOR, y=CLARITY, fill=KPI), 
+  layer(data=data_final, 
+        mapping=aes(x="REF_USA", y=as.character(ORIGIN_COUNTRY), label=as.character(REF_USA)), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="text",
+        geom_params=list(colour="black", vjust=4), 
+        position=position_identity()
+  ) +
+  layer(data=data_final, 
+        mapping=aes(x="ASY_USA", y=as.character(ORIGIN_COUNTRY), label=as.character(ASY_USA)), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="text",
+        geom_params=list(colour="black", vjust=4), 
+        position=position_identity()
+  ) +
+  layer(data=data_final, 
+        mapping=aes(x="ref per asy GER", y=as.character(ORIGIN_COUNTRY), label=as.character(round(ref_per_asy_GER))), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="text",
+        geom_params=list(colour="black", vjust=4), 
+        position=position_identity()
+  ) +
+  layer(data=data_final, 
+        mapping=aes(x="ref per asy USA", y=as.character(ORIGIN_COUNTRY), label=as.character(round(ref_per_asy_USA))), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="text",
+        geom_params=list(colour="black", vjust=4), 
+        position=position_identity()
+  ) +
+  layer(data=data_final, 
+        mapping=aes(x="ASY_GER", y=as.character(ORIGIN_COUNTRY), fill=KPI), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="tile",
+        geom_params=list(alpha=0.50), 
+        position=position_identity()
+  ) +
+  layer(data=data_final, 
+        mapping=aes(x="ASY_USA", y=as.character(ORIGIN_COUNTRY), fill=KPI), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="tile",
+        geom_params=list(alpha=0.50), 
+        position=position_identity()
+  ) +
+  layer(data=data_final, 
+        mapping=aes(x="REF_GER", y=as.character(ORIGIN_COUNTRY), fill=KPI), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="tile",
+        geom_params=list(alpha=0.50), 
+        position=position_identity()
+  ) +
+  layer(data=data_final, 
+        mapping=aes(x="REF_USA", y=as.character(ORIGIN_COUNTRY), fill=KPI), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="tile",
+        geom_params=list(alpha=0.50), 
+        position=position_identity()
+  ) +
+  layer(data=data_final, 
+        mapping=aes(x="ref per asy GER", y=as.character(ORIGIN_COUNTRY), fill=KPI), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="tile",
+        geom_params=list(alpha=0.50), 
+        position=position_identity()
+  ) +
+  layer(data=data_final, 
+        mapping=aes(x="ref per asy USA", y=as.character(ORIGIN_COUNTRY), fill=KPI), 
         stat="identity", 
         stat_params=list(), 
         geom="tile",
